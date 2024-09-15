895112c3c33aeaa281e47309a563d91395c5e9bb
shakivhusain <shakiv.husain@hidubai.com>
2024-09-15 15:05:45 +0400

    Imp Compoents

---
+++ src/app/index.jsx
@@ -0,0 +1,10 @@
+import {Text} from "react-native"
+import Greet from "../components/Greet"
+
+function Index(){
+    return <Greet/>
+}
+
+
+
+export default Index
---
+++ src/components/Greet.tsx
@@ -0,0 +1,14 @@
+import { Text } from "react-native"
+
+
+function Greet(){
+    return <Text>Good Day Mr Shakiv HuXn Mansoori</Text>
+}
+
+
+export default Greet;
+
+// Another way of writing function
+// const  Greet= () => { 
+//     return <Text>Good Day Mr Shakiv HuXn</Text>
+// }
