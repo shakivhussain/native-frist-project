fcd54dac0441d89c53b57c4b05ea4ee3204ccdbd
shakivhusain <shakiv.husain@hidubai.com>
2024-09-15 15:54:39 +0400

    Multiple Compoentns Imp

--- src/app/index.jsx
+++ src/app/index.jsx
@@ -1,10 +1,15 @@
-import {Text} from "react-native"
-import Greet from "../components/Greet"
+import { Text, View } from "react-native";
+import Greet from "../components/Greet";
 
-function Index(){
-    return <Greet/>
-}
+import MyName from "../components/MyName";
 
+const Index = () => {
+  return (
+    <View>
+      <MyName />
+      <Greet />
+    </View>
+  );
+};
 
-
-export default Index
+export default Index;
---
+++ src/components/MyName.tsx
@@ -0,0 +1,7 @@
+import { Text } from 'react-native'
+
+const MyName = () => {
+  return <Text>My Name is Shakib </Text>
+}
+
+export default MyName;
