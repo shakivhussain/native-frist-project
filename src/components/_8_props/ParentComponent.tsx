0658f34cf244fb504bd1c8606abd3ab1102f4fd7
shakivhusain <shakibraza436@gmail.com>
2024-09-16 12:22:29 +0400

    Minor Changes

--- src/components/_8_props/ParentComponent.tsx
+++ src/components/_8_props/ParentComponent.tsx
@@ -2,11 +2,12 @@ import { View, Text } from 'react-native'
 import React from 'react'
 import ChildComponent from './ChildComponent'
 
+
 const ParentComponent = () => {
     return (
         <View>
             <Text>Parent Component</Text>
-            
+
             {/* With the help of props we can pass  multiple types argyments. */}
             <ChildComponent name="Shakiv Husain" hobbies={[
                 "Music",
